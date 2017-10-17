7c1399750b0340a59ccc0dc22ee34c473141240d
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2017-10-17 09:33:04 +0200

    minimum log level live is warning now

--- htdocs/typo3conf/ConfigurationProduction.php
+++ htdocs/typo3conf/ConfigurationProduction.php
@@ -8,3 +8,8 @@ $TYPO3_CONF_VARS['BE']['lockSSL'] = 2;
 
 //fatale Fehler werden direkt per Mail gemeldet. Alles ab Warnung läuft ins PHP Error Log zusätzlich zum devlog.
 $TYPO3_CONF_VARS['SYS']['systemLog'] = 'mail,%2$s,4;error_log,,2;syslog,LOCAL0,,3';
+
+$extensionConfigurationMkLog = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['mklog']);
+$extensionConfigurationMkLog['min_log_level'] = \DMK\Mklog\Utility\SeverityUtility::WARNING;
+$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['mklog'] = serialize($extensionConfigurationMkLog);
+unset($extensionConfigurationMkLog);
