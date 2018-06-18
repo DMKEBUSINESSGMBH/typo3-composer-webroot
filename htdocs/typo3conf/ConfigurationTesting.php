53aae33d084148613f8083c65456a4bd3135c393
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2018-06-18 09:39:53 +0200

    cleanup deactivate deprecation log

--- htdocs/typo3conf/ConfigurationTesting.php
+++ htdocs/typo3conf/ConfigurationTesting.php
@@ -13,8 +13,6 @@ $TYPO3_CONF_VARS['SYS']['exceptionalErrors'] = $TYPO3_CONF_VARS['SYS']['errorHan
 $TYPO3_CONF_VARS['SYS']['displayErrors'] = '1';
 $TYPO3_CONF_VARS['SYS']['systemLogLevel'] = '0';
 $TYPO3_CONF_VARS['SYS']['sqlDebug'] = '1';
-$TYPO3_CONF_VARS['SYS']['enableDeprecationLog'] = 'file';
-
 
 $TYPO3_CONF_VARS['SYS']['systemLog'] = 'mail,%2$s,4;error_log,,2;syslog,LOCAL0,,3';
 $TYPO3_CONF_VARS['EXT']['extConf']['rn_base'] = 'a:11:{s:13:"verboseMayday";s:1:"1";s:11:"dieOnMayday";s:1:"1";s:21:"forceException4Mayday";s:1:"1";s:16:"exceptionHandler";s:27:"tx_rnbase_exception_Handler";s:20:"sendEmailOnException";s:%1$d:"%2$s";s:9:"fromEmail";s:17:"noreply@domain.de";s:24:"send503HeaderOnException";s:1:"1";s:17:"loadHiddenObjects";s:1:"0";s:13:"activateCache";s:1:"0";s:18:"activateSubstCache";s:1:"0";s:8:"debugKey";s:9:"dmkhp2014";}';
