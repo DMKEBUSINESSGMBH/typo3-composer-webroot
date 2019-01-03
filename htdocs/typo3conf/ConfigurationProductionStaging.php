4428404c219472736ad645bc4afbc661fbab4c82
Michael Wagner <rengaw83@users.noreply.github.com>
2019-01-03 15:33:24 +0100

    mail laceholder for sprintf added

--- htdocs/typo3conf/ConfigurationProductionStaging.php
+++ htdocs/typo3conf/ConfigurationProductionStaging.php
@@ -9,7 +9,7 @@ call_user_func(
         //6135 = E_ALL & ~(E_STRICT | E_NOTICE | E_DEPRECATED | E_USER_DEPRECATED)
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = 6135;//alles behandeln außer unkritische Meldungen
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = $GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'];//alles als Exception behandeln (Fehlerseite ausgeben, Logging)
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLog'] = 'mail,tegut.warnings@dmkdev.de,4;error_log,,2;syslog,LOCAL0,,3';
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLog'] = 'mail,%1$s,4;error_log,,2;syslog,LOCAL0,,3';
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = '1';
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLogLevel'] = '0';
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = '1';
